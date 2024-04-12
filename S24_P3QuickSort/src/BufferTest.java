3ec18f06c97d90a59b7c9844a9e51d0ddc4adf90
RichardMartinez <rmartinez21@vt.edu>
2024-04-12 16:47:16 -0400

    implement basic buffer class

---
+++ S24_P3QuickSort/src/BufferTest.java
@@ -0,0 +1,46 @@
+import student.TestCase;
+
+/**
+ * This class tests the methods of the Buffer class
+ * 
+ * @author Richard Martinez
+ * 
+ * @version 2024-04-12
+ */
+public class BufferTest extends TestCase {
+
+    private Buffer buf;
+    
+    /**
+     * Set up the test object
+     */
+    public void setUp() {
+        buf = new Buffer(10);
+    }
+    
+    /**
+     * Test the buffer class
+     */
+    public void testBuffer() {        
+        // Test getID
+        assertEquals(buf.getID(), 10);
+        
+        // Test set
+        byte[] record = {(byte)0xAA, (byte)0xBB, (byte)0xCC, (byte)0xDD};
+        buf.set(record, 0);
+        
+        // Test get
+        byte[] space = new byte[4];
+        buf.get(space, 0);
+        assertEquals(space[0], (byte)0xAA);
+        assertEquals(space[1], (byte)0xBB);
+        assertEquals(space[2], (byte)0xCC);
+        assertEquals(space[3], (byte)0xDD);
+        
+        // Test makeDirty
+        buf.makeDirty();
+        
+        // Test isDirty
+        assertTrue(buf.isDirty());
+    }
+}
